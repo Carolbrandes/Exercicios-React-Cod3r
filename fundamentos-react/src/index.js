d9ecf501d2a2b49c3735a455e912f3133cd8e35b
Carol Brandes <programacao@fabiolamolina.com.br>
2020-06-10 21:00:43 -0300

    aula comando render

--- fundamentos-react/src/index.js
+++ fundamentos-react/src/index.js
@@ -1,1 +1,8 @@
- 
+ // vamos interagir com a biblioteca react dom
+ // vamos usar essa biblioteca para renderizar as coisas na tela
+ import ReactDOM from  'react-dom';
+
+ // temos um metodo utilizado para fazer a renderizações que e render()
+ // o render recebe dois parametros, o 1º e o que queremos escrever e o 2º e o elemento que vai conter esse texto
+ const elemento = document.getElementById('root');
+ ReactDOM.render('Ola react', elemento);
