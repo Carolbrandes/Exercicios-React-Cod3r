5544c7e067b2c59a071aa5fefd064aec6be6936a
Carol Brandes <programacao@fabiolamolina.com.br>
2020-06-11 21:06:47 -0300

    aula introducao jsx

--- fundamentos-react/src/index.js
+++ fundamentos-react/src/index.js
@@ -2,7 +2,12 @@
  // vamos usar essa biblioteca para renderizar as coisas na tela
  import ReactDOM from  'react-dom';
 
+ // para o jsx funcionar precisamos importar o react
+ import React from 'react';
+
+ const tag = <strong>Ola react</strong>;
+
  // temos um metodo utilizado para fazer a renderizações que e render()
  // o render recebe dois parametros, o 1º e o que queremos escrever e o 2º e o elemento que vai conter esse texto
  const elemento = document.getElementById('root');
- ReactDOM.render('Ola react', elemento);
+ReactDOM.render(<div>{tag}</div>, elemento);
