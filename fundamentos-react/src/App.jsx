48255d8482d89b25fd14c07a13ed77944a11d7d0
Carol Brandes <programacao@fabiolamolina.com.br>
2020-06-25 16:43:08 -0300

    aula 20

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -1,3 +1,4 @@
+import './App.css';
 import React from 'react';
 import Primeiro from './components/basicos/Primeiro';
 import ComParametro from './components/basicos/ComParametro';
@@ -6,21 +7,24 @@ import Aleatorio from './components/basicos/Aleatorio';
 import Card from './components/layout/Card';
 
 export default _ =>
-        <div id="app">
+        <div id="App">
                 <h1>Fundamentos React </h1>
-                <Card titulo="#04 - Desafio Números Aleatórios:">
-                        <Aleatorio valorMin={2} valorMax={100} />
-                </Card>
 
-                <Card titulo="#03 - Fragmentos:">
-                        <Fragmento />
-                </Card>
+                <div className="Cards">
+                        <Card titulo="#04 - Desafio Números Aleatórios:">
+                                <Aleatorio valorMin={2} valorMax={100} />
+                        </Card>
 
-                <Card titulo="#02 - Com Parâmetro:">
-                        <ComParametro titulo="Situação do Aluno" aluno="Pedro" nota={9.3}></ComParametro>
-                </Card>
+                        <Card titulo="#03 - Fragmentos:">
+                                <Fragmento />
+                        </Card>
 
-                <Card titulo="#01 - Primeiro Componente:">
-                        <Primeiro></Primeiro>
-                </Card>
+                        <Card titulo="#02 - Com Parâmetro:">
+                                <ComParametro titulo="Situação do Aluno" aluno="Pedro" nota={9.3}></ComParametro>
+                        </Card>
+
+                        <Card titulo="#01 - Primeiro Componente:">
+                                <Primeiro></Primeiro>
+                        </Card>
+                </div>
         </div>
