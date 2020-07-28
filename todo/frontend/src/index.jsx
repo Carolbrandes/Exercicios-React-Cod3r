212a8e4aee05cc770e77fc2510741bbdc5925617
Carol <carolbrandes23@gmail.com>
2020-07-28 14:08:34 -0300

    incluir modules no import do font-awesome e bootstrap

--- todo/frontend/src/index.jsx
+++ todo/frontend/src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './main/App'
+import App from './main/app'
 
  /*esse arquivo é o único que vai interagir com a dom da minha pag */
 
